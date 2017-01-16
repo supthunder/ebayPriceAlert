7169ca38e4095701504871f02a4b73d10af09bd4
elisha <ejohn3@umbc.edu>
2017-01-15 21:17:16 -0500

    in progress

--- alert.py
+++ alert.py
@@ -8,12 +8,11 @@ import os
 import time
 from random import randint
 from time import gmtime, strftime
-import math
-import statistics
+from url import *
 
 def avgPrice(url):
     headers = {'User-agent': 'Mozilla/5.0'}
-    r  = requests.get(url,headers=headers)
+    r  = requests.get(url)
     data = r.text
     soup = BeautifulSoup(data,"html.parser")
     soldPrice = []
@@ -21,6 +20,19 @@ def avgPrice(url):
     # amntRegex = re.compile('\W[0-9]*')
     # amntRegex = re.compile('\$.(\d+).(\d+)')
     amntRegex = re.compile('((\d+).(\d+).(\d+)|(\d+).(\d+))')
+
+    count = 0
+    for details in soup.find_all("li",{"id": "item1c7a25e4c1", "r":"1"}):
+        price = details.find_all("li",{"class": "lvprice prc"})
+        link = details.find_all('a')
+        for lin in link:
+            print(lin['href'])
+
+
+
+
+    exit()
+
 
     # Only check items from US + ignore "similar items" 
     count = int((soup.find_all("span",{"class":"rcnt"}))[0].text.replace(",",""))
@@ -40,14 +52,17 @@ def avgPrice(url):
         print("\nSearch to broad! Too many items!")
         exit()
 
-    for soldItems in range(0,count):
-        currentPrice = (re.search(amntRegex,str(itemPrice[soldItems].text).lstrip().rstrip().strip())).group()
-        soldPrice.append(float(currentPrice.replace(",","")))
-        itemNames.append(str(itemName[soldItems].text).encode('utf-8').strip())
+
+    # for soldItems in range(0,count):
+    #     currentPrice = (re.search(amntRegex,str(itemPrice[soldItems].text).lstrip().rstrip().strip())).group()
+    #     soldPrice.append(float(currentPrice.replace(",","")))
+    #     itemNames.append(str(itemName[soldItems].text).encode('utf-8').strip())
         
-        # Note max of 198
-        if soldItems == 198:
-            break;
+    #     print(soldPrice)
+    #     exit()
+    #     # Note max of 198
+    #     if soldItems == 198:
+    #         break;
 
     # soldPrice = list(map(float, soldPrice))
 
@@ -70,27 +85,8 @@ def avgPrice(url):
 
 def main():
     # Get Input:
-    print("This script will find the avg sold $ in the US\n")
-    name = input("Item Name: ")
-    # name.replace(" ","%20s")
-    name.replace(" ","%20")
-    # i.e seiko%20skx007j
-
-    print("\nCondition\n\nNew with tags(0)\nNew without tage(1)\nNew with defects(2)")
-    print("Pre-Owned(3)\nNot Specified(4)\nNew(5)\nUsed(6)")
-    condition = int(input("Enter Correct Number: "))
-    conditionDict = [1000,1500,1750,3000,10,3,4]
-
-    print("Choose min/max price, or press ENTER\n")
-    
-    minPrice = input("Min price: ")
-    maxPrice = input("Max price: ")
-    minMaxString = "&_udlo="+minPrice+"&_udhi="+maxPrice
-
-    # new is 3 used is 4 smh
-    url = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_Complete=1&LH_Sold=1&LH_BIN=1&LH_ItemCondition="+str(conditionDict[condition])+"&_nkw="+name+"&_ipg=200&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684"
-    url += minMaxString
-    print("Url: \n",url)
+    print("This script will tweet when someone beats your lowest price\n")
+    price = "359.44"
 
     avgPrice(url)
 
---
+++ url.py
@@ -0,0 +1,1 @@
+url = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_sop=15&LH_BIN=1&LH_ItemCondition=1000&_nkw=seiko%20skx007j&rt=nc&LH_PrefLoc=1&_trksid=p2045573.m1684"
